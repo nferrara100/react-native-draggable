0b2f15cd8dd85af1702cfe7708bbe44a7dd33f45
Nicholas Ferrara <nferrara100@gmail.com>
2019-09-19 18:39:23 +0100

    style: corrected line breaks

--- Draggable.js
+++ Draggable.js
@@ -1,6 +1,6 @@
 /**
  *	* https://github.com/tongyy/react-native-draggable
- * 
+ *
  */
 
 import React, { Component } from 'react';
@@ -28,7 +28,7 @@ export default class Draggable extends Component {
 				uri: PropTypes.string,
 			}),
 			PropTypes.number
-		]), 
+		]),
 		offsetX:PropTypes.number,
 		offsetY:PropTypes.number,
 		renderColor:PropTypes.string,
@@ -42,7 +42,7 @@ export default class Draggable extends Component {
 		z:PropTypes.number,
 		x:PropTypes.number,
 		y:PropTypes.number
-		
+
 	};
 	static defaultProps = {
 		offsetX : 100,
@@ -65,14 +65,14 @@ export default class Draggable extends Component {
 		super(props, defaultProps);
 		const { pressDragRelease, reverse, onMove } = props;
 		this.state = {
-			pan:new Animated.ValueXY(), 
+			pan:new Animated.ValueXY(),
 			_value:{
-				x: 0, 
+				x: 0,
 				y: 0
 			}
 		};
 
-		this.panResponder = PanResponder.create({		
+		this.panResponder = PanResponder.create({
 			onMoveShouldSetPanResponder: (evt, gestureState) => true,
 			onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
 			onPanResponderGrant: (e, gestureState) => {
@@ -81,7 +81,7 @@ export default class Draggable extends Component {
 					this.state.pan.setValue({x: 0, y: 0});
 				}
 			},
-			onPanResponderMove: Animated.event([null,{ 
+			onPanResponderMove: Animated.event([null,{
 				dx:this.state.pan.x,
 				dy:this.state.pan.y
 			}], {listener: onMove}),
@@ -90,12 +90,12 @@ export default class Draggable extends Component {
 					pressDragRelease(e, gestureState);
 				if(reverse == false)
 					this.state.pan.flattenOffset();
-				else 
+				else
 					this.reversePosition();
-			} 
+			}
 		});
 	}
-	
+
 	_positionCss = () => {
 		let Window = Dimensions.get('window');
 		const { renderSize, offsetX, offsetY, x, y, z } = this.props;
@@ -123,19 +123,19 @@ export default class Draggable extends Component {
 				backgroundColor: renderColor,
 				width: renderSize * 2,
 				height: renderSize * 2,
-				borderRadius: renderSize 
+				borderRadius: renderSize
 			};
 		}else if(renderShape == 'square') {
 			return{
 				backgroundColor: renderColor,
 				width: renderSize * 2,
 				height: renderSize * 2,
-				borderRadius: 0 
+				borderRadius: 0
 			};
 		}else if(renderShape == 'image') {
 			return{
 				width: renderSize,
-				height: renderSize 
+				height: renderSize
 			};
 		}
 	}
@@ -160,18 +160,18 @@ export default class Draggable extends Component {
 	}
 
 	reversePosition = () => {
-		Animated.spring(						
-			this.state.pan,				 
-			{toValue:{x:0,y:0}}		 
+		Animated.spring(
+			this.state.pan,
+			{toValue:{x:0,y:0}}
 		).start();
 	}
 
 	getPosition = () => {
-		return { 
+		return {
 			offsetX: this.state._value.x,
 			offsetY: this.state._value.y,
 			x: this.state._value.x + this.props.x,
-			y: this.state._value.y + this.props.y 
+			y: this.state._value.y + this.props.y
 		};
 	}
 
@@ -181,22 +181,20 @@ export default class Draggable extends Component {
 
 		return (
 			<View style={this._positionCss()}>
-				<Animated.View 
+				<Animated.View
 					{...this.panResponder.panHandlers}
 					style={[this.state.pan.getLayout()]}>
-					<TouchableOpacity 
+					<TouchableOpacity
 						style={this._dragItemCss()}
 						onPress={pressDrag}
 						onLongPress={longPressDrag}
 						onPressIn={pressInDrag}
 						onPressOut={pressOutDrag}
 					>
-						{touchableContent}	
+						{touchableContent}
 					</TouchableOpacity>
 				</Animated.View>
 			</View>
 		);
 	}
 }
-
-
